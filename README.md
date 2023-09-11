d04096ecb63851e4159fd8a562a6b7367d17b75c
ricolajazz <heric.afrimages@gmail.com>
2023-09-11 22:04:35 +0000

    commit before deploy

---
+++ README.md
@@ -0,0 +1,1 @@
+# hl django blog
--- src/blog/settings.py
+++ src/blog/settings.py
@@ -11,21 +11,25 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+env = environ.Env()
+environ.Env.read_env(env_file= str(BASE_DIR / 'blog' / '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yjud43_y9at^elxz#h#ix90wi(mn1s-j^#ub06=a-dd%(&oo_t'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+
+    
 
 
 # Application definition
